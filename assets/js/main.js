807ba2fc62832ee1aa63405b855d2da8790e24e0
DaviLeoMeda <davileo.meda@gmail.com>
2023-03-31 08:47:30 +0200

    NOt working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,49 @@
 button.addEventListener('click', function () {
 
-    const entireWord = document.getElementById('wording');
+    const entireWord = document.getElementById('wording').value;
 
-    let dividedWord = entireWord.value.split(' ');
+    let word = entireWord
 
-    console.log(dividedWord[2]);
+    // console.log(entireWord.length);
 
-    for (let i = 0; i < dividedWord.length; i++) {
-        console.log(dividedWord[i]);
+    // let dividedWord = entireWord.split().reverse().join();
+
+    // console.log(dividedWord);
+
+    for (let i = word.length - 1; i >= 0; i--) {
+
+        console.log(word[i - 1])
+
+        document.querySelector('.InvertedSense').innerHTML = `${word[0]}+${word[1]}+${word[0]}+${word[0]}+${word[0]}`
+
+
+
+
+
+
+        // if (word[i] == entireWord) {
+        //     console.log(`la parola è palindroma`);
+
+        // } else if (word[i] != entireWord) {
+        //     console.log(`la parola non è palindroma`);
+        // }
+
+        // document.querySelector(`.normalSense`).innerHTML += `${entireWord[i]} `;
     }
+
+})
+
+function randomNumber(max, min) {
+    return Math.floor(Math.random() * max) + min;
+}
+
+buttonB.addEventListener('click', function ({
+
+    let numberX = document.getElementById('numbering').value;
+
+let cpu = randomNumber(10, 1)
+
+console.log(humanFav, cpu);
+
 
 })
